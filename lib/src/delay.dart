8a18df7d4f0f590366def2c4680151ac00b91fe7
Marcin Wróblewski <marcin.wroblewski@monterail.com>
2025-02-07 08:41:00 +0100

    fix: docs according to linter warning, throttle test

--- lib/src/delay.dart
+++ lib/src/delay.dart
@@ -1,6 +1,6 @@
 part of '../bloc_event_transformers.dart';
 
-/// The delay() transformer is pausing adding events for a particular
+/// The `delay()` transformer is pausing adding events for a particular
 /// increment of time (that you specify) before emitting each of the events.
 /// This has the effect of shifting the entire sequence of
 /// events added to the bloc forward in time by that specified increment.
